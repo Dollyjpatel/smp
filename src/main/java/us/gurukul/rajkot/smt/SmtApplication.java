dddaeec57a8abb8e25734a6fed5a48ea8b9834a2
Dolly Patel <dollyjpatel@gmail.com>
2020-08-19 07:50:44 -0500

    initial commit

---
+++ src/main/java/us/gurukul/rajkot/smt/SmtApplication.java
@@ -0,0 +1,13 @@
+package us.gurukul.rajkot.smt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/us/gurukul/rajkot/smt/SmtApplicationTests.java
@@ -0,0 +1,13 @@
+package us.gurukul.rajkot.smt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
